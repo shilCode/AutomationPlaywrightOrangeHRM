9af0f11c1110a6691d0850209c160c149b71173b
shilcode <2850542@gmail.com>
2026-02-08 16:11:41 +0100

    hardcoding

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -9,8 +9,8 @@ test.beforeEach(async ({ page }) => {
   expect(page.url()).toMatch(/auth/);
   const login = new LoginPage(page);
   //login
-  await login.userName.fill(process.env.USERNAME!); //TODO: add @type/nodes to bypass !
-  await login.password.fill(process.env.PASSWORD!); //TODO: add @type/nodes to bypass !
+  await login.userName.fill(process.env.USERNAME!||'Admin'); //TODO: add @type/nodes to bypass !
+  await login.password.fill(process.env.PASSWORD!||'Admin123'); //TODO: add @type/nodes to bypass !
   await login.submit.click();
 });
 
